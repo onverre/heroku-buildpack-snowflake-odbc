06885485a95198eff14805bdca4b6aaa33c4f9b5
John Thomas <thomas07@vt.edu>
2024-02-24 08:13:09 -0500

    some updates

--- .profile.d/configure-snowflake-odbc.sh
+++ .profile.d/configure-snowflake-odbc.sh
@@ -6,11 +6,4 @@ mkdir -p /etc
 echo "[snowflake]
 Description=Snowflake Database
 Driver=SnowflakeDSIIDriver
-uid         = ${SNOWFLAKE_USERNAME}
-pwd         = ${SNOWFLAKE_PASSWORD}
-server      = ${SNOWFLAKE_ACCOUNT}.snowflakecomputing.com
-database    = ${SNOWFLAKE_DATABASE}
-schema      = ${SNOWFLAKE_SCHEMA}
-warehouse   = ${SNOWFLAKE_WAREHOUSE}
-role        = ${SNOWFLAKE_ROLE}
 " > ${ODBCSYSINI}/odbc.ini
