6dad4f15ead15ceb72cb867c6572f5d3f1f90dc4
John Thomas <thomas07@vt.edu>
2024-02-24 15:41:34 -0500

    test

--- .profile.d/configure-snowflake-odbc.sh
+++ .profile.d/configure-snowflake-odbc.sh
@@ -5,5 +5,5 @@ export ODBCSYSINI=${HOME}/.apt/usr/lib/snowflake/odbc/conf
 mkdir -p /etc
 echo "[snowflake]
 Description=Snowflake Database
-Driver=SnowflakeDSIIDriver
+Driver=/app/.apt/usr/lib/snowflake/odbc/lib/libSnowflake.so
 " > ${ODBCSYSINI}/odbc.ini
